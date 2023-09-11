46e28f3d430adfe80bb75b769da860714d0b7eba
Gaurav-822 <gaurav.moocs@gmail.com>
2023-09-11 19:38:15 +0530

    Supported with images

--- app/src/main/java/com/example/lates/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/lates/adapter/ItemAdapter.kt
@@ -1,10 +1,12 @@
 package com.example.lates.adapter
 
 import android.content.Context
+import android.media.Image
 import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.lates.R
@@ -17,6 +19,7 @@ class ItemAdapter(
 
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView: ImageView = view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -33,5 +36,6 @@ class ItemAdapter(
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         holder.textView.text = context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imageResourceId)
     }
 }
