141d3f8821543b40924c0c89dce87e62b61204d9
Gaurav-822 <gaurav.moocs@gmail.com>
2023-09-13 21:17:47 +0530

    Running images from url in recycler view!

--- app/src/main/java/com/example/lates/data/Datasource.kt
+++ app/src/main/java/com/example/lates/data/Datasource.kt
@@ -7,21 +7,14 @@ import com.example.lates.model.News
 
 
 class Datasource {
-    fun urlFormatter(url: Int): String {
+    private fun urlFormatter(url: Int): String {
         return Resources.getSystem().getString(url)
     }
     fun loadNews(): List<News> {
         return listOf<News> (
-            News(com.example.lates.R.string.news1, urlFormatter(R.string.url1)),
+            News(R.string.news1, urlFormatter(R.string.url1)),
             News(R.string.news2, urlFormatter(R.string.url2)),
-//            News(R.string.news3, R.drawable.image3),
-//            News(R.string.news4, R.drawable.image4),
-//            News(R.string.news5, R.drawable.image5),
-//            News(R.string.news6, R.drawable.image6),
-//            News(R.string.news7, R.drawable.image7),
-//            News(R.string.news8, R.drawable.image8),
-//            News(R.string.news9, R.drawable.image9),
-//            News(R.string.news10, R.drawable.image10)
+            News(R.string.news3, urlFormatter(R.string.url1)),
         )
     }
 }
